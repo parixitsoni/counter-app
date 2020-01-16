a8413b31fdd73a8ec57499ed694e7f846c1552b7
parixitsoni <parikshitsoni85@gmail.com>
2020-01-16 09:21:53 +0530

    Update counters.jsx
    Add map function

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,15 +5,17 @@ class Counters extends Component {
     state = { 
         counters:[ 
             { id: 1, value: 0 },
-            { id: 2, value: 1 },
+            { id: 2, value: 0 },
             { id: 3, value: 0 },
             { id: 4, value: 0 }
         ]
-     }
+     };
     render() { 
         return (  
             <div>
-                {this.state.counters.map (counter => <Counter key={counter.id} />)}
+                {this.state.counters.map (counter => (
+                <Counter key={counter.id} value={counter.value} />
+                ))}
             </div>
             
         );
