7b284c897c70b2e0d106e86d3ec5e115dfaec120
parixitsoni <parikshitsoni85@gmail.com>
2020-01-16 07:12:53 +0530

    Create counter.jsx
    create component folder and add counter.jsx file

---
+++ src/components/counter.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {  
+        count:0
+        };
+
+    handleIncrement = () => {
+        this.setState({count: this.state.count + 1 })
+    }
+
+    render() { 
+        return (  
+            <div>
+                <span className={this.getBadgeClassess()}>{this.formatCount()}</span>
+                <button className='badge badge-secondary btn-sm' onClick={this.handleIncrement}>Increment</button>
+            </div>
+        );
+    }
+    getBadgeClassess() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
+    }
+
+formatCount(){
+    const { count } = this.state
+    return count === 0 ? 'Zero' : count;
+
+}
+}
+ 
+export default Counter;
