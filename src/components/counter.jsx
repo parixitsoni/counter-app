e8e24474d567e2b87f465c5b0880e64dd851e84e
parixitsoni <parikshitsoni85@gmail.com>
2020-01-17 00:24:53 +0530

    Update counter.jsx
    add Increment and delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,32 +1,24 @@
 import React, { Component } from 'react';
 
+
 class Counter extends Component {
-    state = {  
-        value:this.props.value
-        };
-
-    handleIncrement = () => {
-        this.setState({value: this.state.value + 1 })
-    }
-
-    render() { 
-        console.log('props',this.props);
-        return (  
-            <div>
+     render() { 
+          return (  
+            <div>   
                 <span className={this.getBadgeClassess()}>{this.formatCount()}</span>
-                <button className='btn btn-success btn-sm' onClick={this.handleIncrement}>Increment</button>
-                <button className="btn btn-danger btn-sm m-2">Delete</button>
+                <button className='btn btn-success btn-sm' onClick={() => this.props.onIncrement(this.props.counter)}>Increment</button>
+                <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
             </div>
         );
     }
     getBadgeClassess() {
         let classes = 'badge m-2 badge-';
-        classes += (this.state.value === 0) ? 'warning' : 'primary';
+        classes += (this.props.counter.value === 0) ? 'warning' : 'primary';
         return classes;
     }
 
 formatCount(){
-    const { value } = this.state
+    const { value } = this.props.counter;
     return value === 0 ? 'ZER0' : value;
 
 }
