11d1990b99005077f2a19509d6cf403bbfcd0478
parixitsoni <parikshitsoni85@gmail.com>
2020-01-16 08:03:56 +0530

    Update App.js
    change import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Counter from './components/counter';
+import Counters from './components/counters';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter />
+        <Counters />
       </div>
     );
   }
