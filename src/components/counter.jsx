54a67222a1fe15d36a30882d0b9a365725b99b6e
parixitsoni <parikshitsoni85@gmail.com>
2020-01-16 09:22:12 +0530

    Update counter.jsx
    create Delete btn class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,30 +2,32 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {  
-        count:0
+        value:this.props.value
         };
 
     handleIncrement = () => {
-        this.setState({count: this.state.count + 1 })
+        this.setState({value: this.state.value + 1 })
     }
 
     render() { 
+        console.log('props',this.props);
         return (  
             <div>
                 <span className={this.getBadgeClassess()}>{this.formatCount()}</span>
-                <button className='btn btn-secondary btn-sm' onClick={this.handleIncrement}>Increment</button>
+                <button className='btn btn-success btn-sm' onClick={this.handleIncrement}>Increment</button>
+                <button className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }
     getBadgeClassess() {
         let classes = 'badge m-2 badge-';
-        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        classes += (this.state.value === 0) ? 'warning' : 'primary';
         return classes;
     }
 
 formatCount(){
-    const { count } = this.state
-    return count === 0 ? 'Zero' : count;
+    const { value } = this.state
+    return value === 0 ? 'ZER0' : value;
 
 }
 }
